110260ec9cd108c1d6e36ee35f8e96ce7c49aa21
vishnu4518 <vishnushrijibaba@gmail.com>
2024-05-30 18:25:57 +0530

    updated

---
+++ api/index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const multer = require("multer");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
+const path = require("path");
+const cors = require("cors");
+
+dotenv.config();
+
+// Connect to MongoDB
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
+.then(() => {
+  console.log("Connected to MongoDB");
+})
+.catch((error) => {
+  console.error("MongoDB connection error:", error);
+  process.exit(1); // Exit the process if MongoDB connection fails
+});
+
+// Serve static files
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use(cors()); // Add CORS middleware
+
+// Multer storage configuration
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// File upload route
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// Routes
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+
+const PORT = process.env.PORT || 8800; // Use environment variable for port or default to 8800
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Backend server is running on port ${PORT}`);
+});
