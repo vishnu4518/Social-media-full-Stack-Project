c547e66a9c9533796be9080ec39a98a7e7740318
vishnu4518 <145912043+vishnu4518@users.noreply.github.com>
2024-05-31 23:11:43 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 
-const rootElement = document.getElementById("root");
-
-if (!rootElement) {
-  throw new Error("Root element not found");
-}
-
-ReactDOM.unstable_createRoot(rootElement).render(
+ReactDOM.render(
   <React.StrictMode>
     <AuthContextProvider>
       <App />
     </AuthContextProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
